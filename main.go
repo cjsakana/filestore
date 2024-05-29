dc77cca3ce1d002f1c9d7b7ee256b2dc4c577dfc
cjsakana <124485452+cjsakana@users.noreply.github.com>
2024-05-29 09:48:25 +0800

    Update main.go

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	http.HandleFunc("/file/update", handler.FileMetaUpdateHandler)
 
 	http.HandleFunc("/user/signup", handler.SignupHandler)
-	http.HandleFunc("POST /user/signin", handler.SignInHandler)
+	http.HandleFunc("/user/signin", handler.SignInHandler)
 	http.HandleFunc("/user/info", handler.HTTPInterceptor(handler.UserInfoHandler))
 
 	err := http.ListenAndServe(":8080", nil)
